daf6ca1cc2b8e5a902bb70faa7ee5ad074d1eb74
feeblamer <adeacea@gmail.com>
2025-10-20 22:47:18 +0500

    Add Input data as CSV file

--- main.py
+++ main.py
@@ -1,5 +1,31 @@
+import click
+import csv
+import logging
+
+INPUT_DATA = "input_data.csv"
+
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.NOTSET)
+
+logger = logging.getLogger("cubic_splines")
+logger.setLevel(logging.DEBUG)
+
+def read_data(file_name):
+    with open(file_name, 'r', encoding='utf-8') as f:
+        reader = csv.DictReader(f, delimiter=',')
+        result = [(float(row["x"]), float(row["y"])) for row in reader]
+        return result
+
 def main():
-    print("Hello from cubicsplines!")
+    input_data = read_data(INPUT_DATA)
+    logger.debug(f"Input data: {input_data}")
+
+
+@click.command()
+@click.option('--input-data', prompt='Введите ваше имя')
+def hello(input_data):
+    click.echo(f'Привет, {input_data}!')
+
+
 
 
 if __name__ == "__main__":
