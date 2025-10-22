d22025c6b9b1c7f88523f4abf749247a254cdd8d
AMaslyakov <a.v.masliakov@urfu.me>
2025-10-22 10:30:32 +0500

    Рефакторинг

--- splines.py
+++ splines.py
@@ -3,6 +3,14 @@
 
 # Поиск a_i
 import logging
+from collections import namedtuple
+
+Segment = namedtuple("Segment", ["x_l", "x_r"])
+
+Coefficient_c_i = namedtuple("Coefficient_c_i", ["p", "q", "l", "s"])
+
+CoefficientPolynom = namedtuple("CoefficientPolynom", ["a", "b", "c", "d"])
+
 
 logger = logging.getLogger("spline-lib")
 logging.basicConfig(level=logging.DEBUG,
@@ -13,17 +21,18 @@ def select_segments(x):
     length = len(x)
     segments = []
     for i in range(length - 1):
-        segment = (x[i], x[i+1])
+        # segment = (x[i], x[i+1])
+        segment = Segment(x[i], x[i+1])
         segments.append(segment)
     return segments
 
 
-def calculate_h_i(segments):
+def calculate_h_i(segments:list[Segment]):
     h_i = []
     logger.debug(f"{segments=}")
     for segment in segments:
         logger.debug(f"{segment=}")
-        diff = segment[1] - segment[0]
+        diff = segment.x_r - segment.x_l
         h_i.append(diff)
     return h_i
 
@@ -32,14 +41,13 @@ def get_a_i(y) -> list[float]:
    return y[:-1]
 
 
-def tridiagonal_coefficients(input_data):
-    x = list(map(lambda c: c[0], input_data))
-    segments = select_segments(x)
+def tridiagonal_coefficients(y, h_i)-> list[Coefficient_c_i]:
+    '''
+        
+    '''
 
     #n - количество отрезков и уравнений в системе
-    n = len(segments)
-    y = list(map(lambda c: c[1], input_data))
-    h_i = calculate_h_i(segments)
+    n = len(h_i)
 
     coefficients = []
     for i in range(1, n):
@@ -47,43 +55,63 @@ def tridiagonal_coefficients(input_data):
         down_diagonal = h_i[i - 1]
         upper_diagonal = h_i[i]
 
-        # в массиве H индексы начинаются с 0, как и в массиве y
-        # поэтому индексы массива Y понижены на 1 для того чтобы формула коэффицентов значений, была корректной
+        # в массиве h индексы начинаются с 0, как и в массиве y
+        # поэтому индексы массива Y понижены на 1 для того чтобы формула коэффициентов значений, была корректной
         answer = 3 * (((y[i + 1] - y[i]) / h_i[i]) - ((y[i] - y[i - 1]) / h_i[i - 1]))
-        #logger.debug(f"{main_diagonal=}; {down_diagonal=}; {upper_diagonal=}; {answer=}")
         if i == 1:
-            coefficients.append({"p":0, "q": main_diagonal, "l": upper_diagonal, "s": answer})
+            coeff = Coefficient_c_i(0, main_diagonal, upper_diagonal, answer)
+            # coeff = {"p":0,
+            #          "q": main_diagonal,
+            #          "l": upper_diagonal,
+            #          "s": answer}
+            # coefficients.append({"p":0, "q": main_diagonal, "l": upper_diagonal, "s": answer})
         elif i == n - 1:
-            coefficients.append({"p":down_diagonal, "q": main_diagonal, "l": 0, "s": answer})
+            coeff = Coefficient_c_i(down_diagonal, main_diagonal, 0, answer)
+            # coeff = {"p":down_diagonal,
+            #          "q": main_diagonal,
+            #          "l": 0,
+            #          "s": answer}
+            # coefficients.append({"p":down_diagonal, "q": main_diagonal, "l": 0, "s": answer})
         else:
-            coefficients.append({"p":down_diagonal, "q": main_diagonal, "l": upper_diagonal, "s": answer})
+            coeff = Coefficient_c_i(down_diagonal, main_diagonal, upper_diagonal, answer)
+            # coeff = {"p":down_diagonal,
+            #          "q": main_diagonal,
+            #          "l": upper_diagonal,
+            #          "s": answer}
+            # coefficients.append({"p":down_diagonal, "q": main_diagonal, "l": upper_diagonal, "s": answer})
+        coefficients.append(coeff)
     return coefficients
 
-def calculate_U(coefficients):
-    u1 = -coefficients[0]["l"] / coefficients[0]["q"]
+def calculate_U(coefficients: list[Coefficient_c_i]):
+    u1 = -coefficients[0].l/ coefficients[0].q
     U = [u1]
     for i, coeff in enumerate(coefficients[1:], start=1):
-        u_i = -coeff["l"]/(coeff["p"] * U[i-1] + coeff["q"])
+        u_i = -coeff.l/(coeff.p * U[i-1] + coeff.q)
         U.append(u_i)
     return U
 
 
 def calculate_V(coefficients):
-    v1 = coefficients[0]["s"]/coefficients[0]["q"]
+    v1 = coefficients[0].s/coefficients[0].q
     V = [v1]
     U = calculate_U(coefficients)
     for i, coeff in enumerate(coefficients[1:], start=1):
-       v_i = (coeff["s"] - coeff["p"]*V[i-1])/(coeff["p"]*U[i-1] + coeff["q"])
+       v_i = (coeff.s - coeff.p * V[i-1])/(coeff.p * U[i-1] + coeff.q)
        V.append(v_i)
     return V
 
 
-def calculate_c_i(U:list[float], V:list[float]):
+def calculate_c_i(y, h_i):
     """
-    :param U: Коэффициенты прогонки U
-    :param V: Коэффициенты прогонки V
+    Нахождение коэффициентов c_i кубических полиномов
+    :param y:
+    :param h_i:
     :return: коэффициенты кубического полинома c_i
     """
+    coefficients = tridiagonal_coefficients(y, h_i)
+    U = calculate_U(coefficients)
+    V = calculate_V(coefficients)
+
     n = len(U)
     c = [0.0]  # c_0 = 0 для естественного сплайна
     
@@ -127,7 +155,7 @@ def create_spline_function(a_coeff, b_coeff, c_coeff, d_coeff, x_start):
     return spline_function
 
 def assembly_cubic_polynomial(x, a, b, c, d):
-    segments = select_segments(x)
+    segments:list[Segment] = select_segments(x)
     functions = []
     for i, segment in enumerate(segments):
         # Создаем замыкание с зафиксированными коэффициентами
@@ -138,14 +166,11 @@ def assembly_cubic_polynomial(x, a, b, c, d):
 def build_cubic_spline(input_data, step_x=0.01):
     y = list(map(lambda c: c[1], input_data))
     x = list(map(lambda c: c[0], input_data))
-    segments = select_segments(x)
+    segments:list[Segment] = select_segments(x)
     h_i = calculate_h_i(segments)
-    coefficients = tridiagonal_coefficients(input_data)
-    U = calculate_U(coefficients)
-    V = calculate_V(coefficients)
 
     a_i = get_a_i(y)
-    c_i = calculate_c_i(U, V)
+    c_i = calculate_c_i(y, h_i)
     d_i = calculate_d_i(c_i, h_i)
     b_i = calculate_b_i(c_i, h_i, y)
 
@@ -154,16 +179,13 @@ def build_cubic_spline(input_data, step_x=0.01):
     result = []
     x_coordinate = x[0]
     for polynom in polynomials:
-        segment = polynom[0]
+        segment:Segment = polynom[0]
         cubic_polynom = polynom[1]
-        while segment[0] <= x_coordinate < segment[1]:
+        while segment.x_l <= x_coordinate < segment.x_r:
             y_coordinate = cubic_polynom(x_coordinate)
             result.append((x_coordinate, y_coordinate))
             x_coordinate += step_x
-
     return result
-
-
 
 
 if __name__ == '__main__':
